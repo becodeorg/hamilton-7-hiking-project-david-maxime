2b3071a263155ce027a1df96688f828a1bd073bb
Abranka <davidabranka@gmail.com>
2022-12-21 16:23:24 +0100

    tailwind cdn instead of full install

--- src/views/header.view.php
+++ src/views/header.view.php
@@ -5,10 +5,12 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script src="https://cdn.tailwindcss.com"></script>
     <title>Hiking Manager</title>
 </head>
 <body>
 <header>
+    <h1 class="text-blue-900 text-3xl">Hiking Manager</h1>
     <nav>
         <ul>
             <li><a href="/">Home</a></li>
