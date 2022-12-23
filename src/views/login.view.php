0c0d453b6ab19648970094fa293aed66cb2f64cd
DAbranka <d.abranka@hotmail.com>
2022-12-23 10:15:51 +0100

    Login Style - Update !

--- src/views/login.view.php
+++ src/views/login.view.php
@@ -11,23 +11,27 @@
 
 <body>
     <main class="flex justify-center items-center  h-screen">
-        <section class="container bg-yellow-500 w-[30rem] h-[48rem] rounded-3xl flex justify-center items-center">
-            <div class="content bg-red-600 w-[26rem] h-[36rem] flex flex-col justify-center items-center">
+
+        <!-- CONTAINER -->
+        <section class="container bg-slate-300 w-[30rem] h-[48rem] rounded-3xl flex justify-center items-center">
+
+        <!-- CONTENT -->
+            <div class="content w-[26rem] h-[36rem] flex flex-col justify-center items-center">
 
                 <!-- TITLE - LOGIN -->
                 <h2 class="text-5xl font-text-red-900 mb-8 font-bold uppercase">Login</h2>
 
                 <!-- FORM -->
-                <form action="login" method="post">
-                    <div>
-                        <label for="nickname">Nickname</label>
-                        <input type="text" name="nickname" class="border-2 border-green-900">
+                <form action="login" method="post" class=" h-48 w-full flex flex-col justify-between">
+                    <div class="">
+                        <!-- <label for="nickname" class="invisible">Nickname</label> -->
+                        <input type="text" name="nickname" placeholder="Nickname" class="border hover:border-2 border-green-900 w-full h-12 rounded-lg px-4">
                     </div>
                     <div>
-                        <label for="password">Password</label>
-                        <input type="password" name="password" class="border-2 border-green-900">
+                        <!-- <label for="password" class="invisible">Password</label> -->
+                        <input type="password" name="password" placeholder="Password" class="border hover:border-2 border-green-900 w-full h-12 rounded-lg px-4">
                     </div>
-                    <button type="submit" class="bg-green-900 text-white">Login !</button>
+                    <button type="submit" class="bg-green-900 hover:bg-slate-900 text-white text-xl font-bold w-full h-12 rounded-full">Login !</button>
                 </form>
             </div>
         </section>
